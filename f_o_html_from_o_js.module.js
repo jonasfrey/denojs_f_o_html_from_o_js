1129c6d8bd89f131b8cdeb12559e49ed6c207483
legion5pro <you@example.com>
2023-02-25 23:10:09 +0100

    added possibility to re-render object

--- f_o_html_from_o_js.module.js
+++ f_o_html_from_o_js.module.js
@@ -11,6 +11,12 @@ var f_o_html_from_o_js = function(
     o_js
 ){
     // console.log(o_js)
+    var o_js_outer = o_js;
+
+    if(typeof o_js.f_o_js  == "function"){
+        o_js = o_js.f_o_js();
+    }
+
     if(!f_b_is_js_object(o_js)){
         // o_js has to be object, 
         return null
@@ -22,6 +28,10 @@ var f_o_html_from_o_js = function(
     var o_html = document.createElement(s_tag);
     var a_s_prop_name = Object.keys(o_js);
     for(var s_prop_name of a_s_prop_name){
+        if(s_prop_name.indexOf("_") == 0){
+            continue
+        }
+
         var value = o_js[s_prop_name];
         
         if(Array.isArray(value)){
@@ -36,15 +46,29 @@ var f_o_html_from_o_js = function(
             !Array.isArray(value) 
               && !f_b_is_js_object(value)
         ){
-            o_html[s_prop_name] = value;
+
+            if(typeof value == "function"){
+                o_html[s_prop_name] = function(){
+                    value.call(this, o_js);
+                } 
+            }
             if(typeof value != 'function'){
+                o_html[s_prop_name] = value;
                 o_html.setAttribute(s_prop_name, value);
             }
         }
         // o_html.addEventListener(s_prop_name, value);
 
     }
-
+    o_js._o_html = o_html;
+    var _f_render = function(){
+        var o_html_old = o_js._o_html;
+        o_js._o_html = f_o_html_from_o_js(this);
+        o_html_old.parentElement.replaceChild(o_js._o_html, o_html_old);
+    }
+    o_js._f_render = _f_render;
+    o_js_outer._f_render = _f_render;
+    o_js_outer._o_html = o_html;
     return o_html;
 }
 
