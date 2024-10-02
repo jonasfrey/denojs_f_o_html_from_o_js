8c5f7f1894dadd54819f7bc0c62a3b92d70f199a
jonas.frey@unibe.ch <jonas.frey@unibe.ch>
2024-10-02 14:55:09 +0200

    asdf

--- localhost/jsh_modules/translatable_scalable_window/mod.js
+++ localhost/jsh_modules/translatable_scalable_window/mod.js
@@ -25,38 +25,28 @@ let s_class = 'overlay_window';
 let s_uuid_mod_scope = crypto.randomUUID();
 let f_o_js = function(
     a_o_js = [], 
-    o_state = {}
+    o_state__input = {}
 ){
     let s_uuid_func_scope = crypto.randomUUID(); 
     let a_s_class = [s_class, s_uuid_mod_scope, s_uuid_func_scope]
-    let o1 = {
-        o_scl: (o_state?.o_scl) ? o_state.o_scl : {n_x: 100, n_y: 100},
-        o_trn: (o_state?.o_trn) ? o_state.o_trn : {n_x: 100, n_y: 100},
-    };
-    let o = Object.assign(
+
+    let o_state = Object.assign(
         {
             
             b_render: true, 
             b_scale: false, 
             b_translate: false,
             o_pd: {n_x: 0, n_y:0}, 
-            o_scl: o1.o_scl,
-            o_trn: o1.o_trn, 
-            o_scl_pd: Object.assign({}, o1.o_scl),
-            o_trn_pd: Object.assign({}, o1.o_trn),
+            o_scl: {n_x: 100, n_y: 100},
+            o_trn: {n_x: 100, n_y: 100}, 
+            o_scl_pd: {n_x: 0, n_y: 0},
+            o_trn_pd: {n_x: 0, n_y: 0},
             s_style: 'border-radius: 3px', 
             s_class: '',
         }, 
-        o_state
+        o_state__input
     )
-    let f_o_assigned = function(s, o, o2){
-        return Object.assign(
-            o2, 
-            {
-                [s]: o
-            }
-        )[s]
-    }
+
     window.onpointerup = function(){
         o_state.b_scale = false;
         o_state.b_translate = false;
@@ -98,51 +88,46 @@ let f_o_js = function(
 
     }
 
-
-    let o_js = f_o_assigned(
-        'o_js', 
-        {
-            f_o_jsh: async ()=>{
-                console.log('asdf')
-                return {
-                    b_render: o_state.b_render,
-                    class: [
-                        o_state.s_class,
-                        ...a_s_class,
-                    ].join(' '),
-                    style: [
-                        `position: absolute`, 
-                        `left: ${o_state.o_trn.n_x}px`,
-                        `top:${o_state.o_trn.n_y}px`, 
-                        `width:${o_state.o_scl.n_x}px`,
-                        `height: ${o_state.o_scl.n_y}px`,
-                        o_state.s_style 
-                    ].join(';'), 
-                    a_o: [
-                        ...a_o_js
-                        // {innerText: "drag me"}, 
-                        // {
-                        //     s_tag: "button", 
-                        //     innerText: "translate", 
-                        //     onpointerdown: (o_e)=>{
-                        //         f_update_pointerdown_trn(o_e);
-                        //     }
-                        // },
-                        // {
-                        //     s_tag: "button", 
-                        //     innerText: "scale",
-                        //     onpointerdown: (o_e)=>{
-                        //         f_update_pointerdown_scl(o_e);
-                        //     }
-                        // }
-                    ]
-                }
+    o_state.o_js = {
+        f_o_jsh: async ()=>{
+            console.log('asdf')
+            return {
+                b_render: o_state.b_render,
+                class: [
+                    o_state.s_class,
+                    ...a_s_class,
+                ].join(' '),
+                style: [
+                    `position: absolute`, 
+                    `left: ${o_state.o_trn.n_x}px`,
+                    `top:${o_state.o_trn.n_y}px`, 
+                    `width:${o_state.o_scl.n_x}px`,
+                    `height: ${o_state.o_scl.n_y}px`,
+                    o_state.s_style 
+                ].join(';'), 
+                a_o: [
+                    ...a_o_js
+                    // {innerText: "drag me"}, 
+                    // {
+                    //     s_tag: "button", 
+                    //     innerText: "translate", 
+                    //     onpointerdown: (o_e)=>{
+                    //         f_update_pointerdown_trn(o_e);
+                    //     }
+                    // },
+                    // {
+                    //     s_tag: "button", 
+                    //     innerText: "scale",
+                    //     onpointerdown: (o_e)=>{
+                    //         f_update_pointerdown_scl(o_e);
+                    //     }
+                    // }
+                ]
             }
-        },
-       o_state 
-    )
+        }
+    }
 
-    return o_js;
+    return o_state.o_js;
 
 
 }
